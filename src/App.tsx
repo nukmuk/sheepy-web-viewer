344a308a91cc790767ce9933532ee84d7f467765
poju <rasmus.hintzmadsen@gmail.com>
2023-11-27 20:22:12 +0200

    add timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Viewport from "./Viewport.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
 
 function App() {
 
     return (
-        <>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
             {/*<p className={"text-3xl font-bold underline"}>hello</p>*/}
             <Viewport/>
-        </>
+        </ThemeProvider>
     )
 }
 
--- src/Viewport.tsx
+++ src/Viewport.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import {Canvas} from "@react-three/fiber";
 import {DragEvent, useEffect, useState} from "react";
 import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
 import {AnimationParticle, getFrames} from "./FileLoader.tsx";
-import PlayButton from "./PlayButton.tsx";
 import {Perf} from "r3f-perf";
+import {Slider} from "@/components/ui/slider.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
+import {Pause, Play} from "lucide-react";
 
 export default function Viewport() {
     // const [currentFrame, setCurrentFrame] = useState<AnimationParticle[]>([]);
@@ -50,39 +55,48 @@ export default function Viewport() {
 
         const positions = new Float32Array(currFrame.length * 3);
         const colors = new Float32Array(currFrame.length * 3);
+        const sizes = new Float32Array(currFrame.length);
 
         for (let i = 0; i < currFrame.length; i++) {
             // const [x, y, z, b, g, r, s] = currFrame[i];
             positions.set([currFrame[i][0], currFrame[i][1], currFrame[i][2]], i * 3);
             colors.set([currFrame[i][5], currFrame[i][4], currFrame[i][3]], i * 3);
+            sizes.set([currFrame[i][6] * 20], i);
         }
         // test.set([4,5,6], 1);
 
         // console.log("test:", positions);
 
-        return <points>
-            <bufferGeometry attach="geometry">
-                <bufferAttribute
-                    attach={"attributes-position"}
-                    array={positions}
-                    count={positions.length / 3}
-                    itemSize={3}/>
-                <bufferAttribute
-                    attach={"attributes-color"}
-                    array={colors}
-                    count={colors.length / 3}
-                    itemSize={3}/>
-            </bufferGeometry>
-            <pointsMaterial
-                attach={"material"}
-                vertexColors={true}
-                size={0.1}
-                sizeAttenuation
-                transparent={false}
-                alphaTest={0.5}
-                opacity={1.0}
-            />
-        </points>
+        return <>
+            <points>
+                <bufferGeometry attach="geometry">
+                    <bufferAttribute
+                        attach={"attributes-position"}
+                        array={positions}
+                        count={positions.length / 3}
+                        itemSize={3}/>
+                    <bufferAttribute
+                        attach={"attributes-color"}
+                        array={colors}
+                        count={colors.length / 3}
+                        itemSize={3}/>
+                    <bufferAttribute
+                        attach={"size"}
+                        array={sizes}
+                        count={sizes.length}
+                        itemSize={1}/>
+                </bufferGeometry>
+                <pointsMaterial
+                    attach={"material"}
+                    vertexColors={true}
+                    size={0.1}
+                    sizeAttenuation
+                    transparent={false}
+                    alphaTest={0.5}
+                    opacity={1.0}
+                />
+            </points>
+        </>
 
     }
 
@@ -90,17 +104,27 @@ export default function Viewport() {
         setPlaying(prevState => !prevState);
     }
 
-
     return (
         <>
-            <div className={"z-10 absolute text-white"}>
-                <button onClick={handlePlayClick} className={`text-5xl ${(playing ? "text-yellow-500" : "text-green-500")}`}>{playing ? "Pause" : "Play"}</button>
-                {/*<button onClick={handlePlayClick} className={`text-5xl + {playing ? "text-green-500" : text-yellow-500}`}>{playing ? "Pause" : "Play"}</button>*/}
-                <p>Loaded {frames.length} frames</p>
-                <p>Frame: {frame}</p>
-                <p>Playing: {playing.toString()}</p>
+            <div
+                className="flex flex-col justify-start p-4 pb-16 absolute w-screen h-screen text-foreground pointer-events-none gap-8 select-none">
+                <div className="mb-auto">
+                    {/*<button onClick={handlePlayClick} className={`text-5xl + {playing ? "text-green-500" : text-yellow-500}`}>{playing ? "Pause" : "Play"}</button>*/}
+                    <p>Loaded {frames.length} frames</p>
+                    <p>Frame: {frame}</p>
+                    {/*<p>Playing: {playing.toString()}</p>*/}
+                </div>
+                <div className="flex justify-start gap-4">
+                    <Button onClick={handlePlayClick} variant="secondary" size="icon" className="w-12 h-12 pointer-events-auto"
+                        //     className={`text-5xl pointer-events-auto ${(playing ? "text-yellow-500" : "text-green-500")}`}>
+                        // {playing ? "Pause" : "Play"}
+                    >
+                        {playing ? <Pause className="w-8 h-8"/> : <Play className="w-8 h-8"/>}
+                    </Button>
+                    <Slider value={[frame]} className="cursor-pointer pointer-events-auto" min={0} max={frames.length - 1} step={1} onValueChange={v => setFrame(v[0])}/>
+                </div>
             </div>
-            <div className={"absolute inset-0 bg-neutral-950"} onDrop={handleDrop} onDragOver={handleDragOver}>
+            <div className={"absolute inset-0 bg-background -z-10"} onDrop={handleDrop} onDragOver={handleDragOver}>
                 <Canvas>
                     <PerspectiveCamera makeDefault position={[5, 5, 5]}/>
                     <OrbitControls rotateSpeed={.2} panSpeed={.5}/>
