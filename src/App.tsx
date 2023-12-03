281f6ccba06a78916d55c98892cf845dfefa2a0c
poju <rasmus.hintzmadsen@gmail.com>
2023-12-03 19:22:05 +0200

    fix default theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import Viewport from "./Viewport.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 
 function App() {
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+
     return (
-        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <ThemeProvider defaultTheme={theme} storageKey="vite-ui-theme">
             {/*<p className={"text-3xl font-bold underline"}>hello</p>*/}
             <Viewport />
         </ThemeProvider>
